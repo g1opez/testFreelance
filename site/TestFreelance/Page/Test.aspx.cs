f05df7119936309971d3431c519832e97aabb4f7
Gustavo Lopez <gustavo.l@outlook.com>
2014-04-01 00:23:57 -0300

    Added files and folders of the project

---
+++ site/TestFreelance/Page/Test.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Services;
+
+namespace TestFreelance.Page
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(hdnLblBarRed.Value) && !string.IsNullOrEmpty(hdnLblBarBlue.Value) && !string.IsNullOrEmpty(hdnLblBarGreen.Value))
+            {
+                SetColorBar(hdnLblBarRed.Value, hdnLblBarBlue.Value, hdnLblBarGreen.Value);
+            }
+        }
+
+        public void ShowResult(object sender, EventArgs e)
+        {
+            ShowMessage(hdnLblBarRed.Value, hdnLblBarBlue.Value, hdnLblBarGreen.Value);
+        }
+
+        private void SetColorBar(string red, string blue, string green)
+        {
+            // Percentage 
+            sectorRed.Style.Add("width", red + "%");
+            sectorBlue.Style.Add("width", blue + "%");
+            sectorGreen.Style.Add("width", green + "%");
+            // Label Control
+            lblBarRed.InnerText = red + "%";
+            lblBarBlue.InnerText = blue + "%";
+            lblBarGreen.InnerText = green + "%";
+            // Hidden Control
+            hdnLblBarRed.Value = red;
+            hdnLblBarBlue.Value = blue;
+            hdnLblBarGreen.Value = green;
+        }
+
+        public void ShowMessage(string cRed, string cBlue, string cGreen)
+        {
+            lblMessage.InnerText = "The color's value at the bar is: ";
+            lblMessage1.InnerText = "... " + cRed + "%";
+            lblMessage2.InnerText = "... " + cBlue + "%";
+            lblMessage3.InnerText = "... " + cGreen + "%";
+
+            SetColorBar(cRed, cBlue, cGreen);
+        }
+
+        [WebMethod]
+        public static Object ShowMessageAjax(string cRed, string cBlue, string cGreen)
+        {
+            const string mge = "The color's value at the bar is: ";
+            var lblMessage1 = "... " + cRed + "%";
+            var lblMessage2 = "... " + cBlue + "%";
+            var lblMessage3 = "... " + cGreen + "%";
+            var obj = new Dictionary<string, string>
+                          {
+                              {"lblMessage", mge}, 
+                              {"lblMessage1", lblMessage1}, 
+                              {"lblMessage2", lblMessage2}, 
+                              {"lblMessage3", lblMessage3}
+                          };
+
+            return obj;
+        }
+
+        [WebMethod]
+        public static Object SetLanguage(string lang)
+        {
+            switch (lang)
+            {
+                case "ES":
+                    return new Dictionary<string, string>
+                    {                        
+                        {"button1", "Actualizar pagina (cliente)"},
+                        {"button2", "Obtener valores del Server (POST)"},
+                        {"button3", "Obtener valores del Server (AJAX)"},
+                        {"lblFooter", "Realizado por"},
+                        {"langEs","ES"},
+                        {"langUs","IN"},
+                        {"colorRed","Rojo"},
+                        {"colorBlue","Azul"},
+                        {"colorGreen","Verde"}
+                    };
+
+                case "US":
+                    return new Dictionary<string, string>
+                    {                        
+                        {"button1", "Update Page (client)"},
+                        {"button2", "Get values on server side postback"},
+                        {"button3", "Get values on server side"},
+                        {"lblFooter", "Realized by"},
+                        {"langEs","SP"},
+                        {"langUs","US"},
+                        {"colorRed","Red"},
+                        {"colorBlue","Blue"},
+                        {"colorGreen","Green"}
+                    };
+            }
+            return null;
+        }
+    }
+
+}
---
+++ site/TestFreelance/Scripts/Functions.js
@@ -0,0 +1,80 @@
+﻿/*  Page/Test.aspx  */
+function SetColorInBar(cRed, cBlue, cGreen) {
+    $("#sectorRed").css({
+        'width': '' + cRed + '%'
+    });
+    $("#sectorBlue").css({
+        'width': '' + cBlue + '%'
+    });
+    $("#sectorGreen").css({
+        'width': '' + cGreen + '%'
+    });
+}
+
+function ShowPercentage(cRed, cBlue, cGreen) {
+    // Red        
+    $("#lblBarRed").text(parseFloat(cRed).toFixed(2) + '%');
+    $("#hdnLblBarRed").val(parseFloat(cRed).toFixed(2));
+    // Blue
+    $("#lblBarBlue").text(parseFloat(cBlue).toFixed(2) + '%');
+    $("#hdnLblBarBlue").val(parseFloat(cBlue).toFixed(2));
+    // Green
+    $("#lblBarGreen").text(parseFloat(cGreen).toFixed(2) + '%');
+    $("#hdnLblBarGreen").val(parseFloat(cGreen).toFixed(2));
+}
+
+function ShowMessageAjax(cRed, cBlue, cGreen) {
+    $.ajax({
+        type: "POST",
+        url: "/Page/Test.aspx/ShowMessageAjax",
+        data: "{cRed:'" + cRed + "',cBlue:'" + cBlue + "',cGreen:'" + cGreen + "'}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            setMessageAndShow(data);
+        },
+        error: function (data) {
+            if (data.d != undefined) {
+                alert('There was an error.' + data.d);
+            }
+        }
+    });
+};
+
+function setMessageAndShow(data) {
+    $("#lblMessage").text(data.d.lblMessage);
+    $("#lblMessage1").text(data.d.lblMessage1);
+    $("#lblMessage2").text(data.d.lblMessage2);
+    $("#lblMessage3").text(data.d.lblMessage3);
+}
+
+function SetLanguage(lang) {
+    $.ajax({
+        async: false,
+        type: "POST",
+        url: "/Page/Test.aspx/SetLanguage",
+        data: "{lang:'" + lang + "'}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            $('#btnUpdatePageFromCliente').val(data.d.button1);
+            $('#btnGetValueWithPostBack').val(data.d.button2);
+            $('#btnGetValueWithAjax').val(data.d.button3);
+            $('#langEs').val(data.d.langEs);
+            $('#langUs').val(data.d.langUs);            
+            $('footer > span').text(data.d.lblFooter);
+            $('#divInputColors span:nth-of-type(1)').text(data.d.colorRed);
+            $('#divInputColors span:nth-of-type(2)').text(data.d.colorBlue);
+            $('#divInputColors span:nth-of-type(3)').text(data.d.colorGreen);
+        },
+        error: function (data) {
+            if (data.d != undefined) {
+                alert('There was an error.' + data.d);
+            }
+        }
+    });
+}
+/* --------------- */
+function CalculatePercentage(valueColor, valueTotal) {
+    return valueColor * 100 / valueTotal;
+}
